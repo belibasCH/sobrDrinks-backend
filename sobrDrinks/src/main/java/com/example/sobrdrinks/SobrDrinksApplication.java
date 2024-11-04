6317cd880ad920c485131104ed0158819db3a1b6
Elias Braem <elias.braem@software-brauerei.ch>
2024-11-04 14:39:19 +0100

    initial springboot project

---
+++ sobrDrinks/src/main/java/com/example/sobrdrinks/SobrDrinksApplication.java
@@ -0,0 +1,13 @@
+package com.example.sobrdrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobrDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobrDrinksApplication.class, args);
+	}
+
+}
---
+++ sobrDrinks/src/test/java/com/example/sobrdrinks/SobrDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sobrdrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobrDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
